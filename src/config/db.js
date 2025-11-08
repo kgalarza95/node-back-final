e6d52e10e6323e3e050d48a5c7bc94a515c8a2d5
kgalarza95 <kevin_galata@hotmail.com>
2025-11-08 11:07:54 -0500

    se agregan cambios de configuracion a abse remota

--- src/config/db.js
+++ src/config/db.js
@@ -2,7 +2,8 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URI);
+        //await mongoose.connect(process.env.MONGO_URI);
+        await mongoose.connect("mongodb+srv://jacobogarcesoquendo:aFJzVMGN3o7fA38A@cluster0.mqwbn.mongodb.net/kevingalarza");
         console.log("Conectado a MongoDB");
     } catch (error) {
         console.error("Error al conectar a MongoDB:", error.message);
