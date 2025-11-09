58c078be41ed9a20c45bb7e7364b70a3f879504d
kgalarza95 <kevin_galata@hotmail.com>
2025-11-09 18:55:02 -0500

    se agregan mejoras

--- src/config/db.js
+++ src/config/db.js
@@ -1,9 +1,11 @@
 import mongoose from "mongoose";
 
+const MONGO_URI = "mongodb+srv://jacobogarcesoquendo:aFJzVMGN3o7fA38A@cluster0.mqwbn.mongodb.net/kevingalarza";
+
 export const connectDB = async () => {
     try {
         //await mongoose.connect(process.env.MONGO_URI);
-        await mongoose.connect("mongodb+srv://jacobogarcesoquendo:aFJzVMGN3o7fA38A@cluster0.mqwbn.mongodb.net/kevingalarza");
+        await mongoose.connect(MONGO_URI);
         console.log("Conectado a MongoDB");
     } catch (error) {
         console.error("Error al conectar a MongoDB:", error.message);
